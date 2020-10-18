d0df4906aede553e674e8831203dd5acf061bc0d
hhharsha90 <siddesh2009@gmail.com>
2020-10-19 02:00:34 +0530

    Updates

---
+++ precache-manifest.91a39db7133a3ca0ecc5d5b78cf48fae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0d416fc2cd4be8894c022b6c4f8bdd",
+    "url": "/ImageV1/index.html"
+  },
+  {
+    "revision": "941b4c963a4a12230368",
+    "url": "/ImageV1/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "6d5532037c4e0b9883f1",
+    "url": "/ImageV1/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "941b4c963a4a12230368",
+    "url": "/ImageV1/static/js/2.804e74fa.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/ImageV1/static/js/2.804e74fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d5532037c4e0b9883f1",
+    "url": "/ImageV1/static/js/main.29f91972.chunk.js"
+  },
+  {
+    "revision": "6c5a1fea511d968aaedc",
+    "url": "/ImageV1/static/js/runtime-main.da2498dd.js"
+  },
+  {
+    "revision": "f27ce28238ee20bc1e11a2e2c9a136bc",
+    "url": "/ImageV1/static/media/flower.f27ce282.jpg"
+  },
+  {
+    "revision": "ea7a359227912339a1104b03c69e85b2",
+    "url": "/ImageV1/static/media/image.ea7a3592.jpg"
+  },
+  {
+    "revision": "d73dd0b67391a113150eb5661f8ff00d",
+    "url": "/ImageV1/static/media/img.d73dd0b6.jpg"
+  }
+]);
